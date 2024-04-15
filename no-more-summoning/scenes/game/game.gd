b86c196c383ce11e4f413904fa8111efca321de6
anthonny <anthonny.querouil@gmail.com>
2024-04-16 01:19:22 +0200

    last commit for this LD

--- no-more-summoning/scenes/game/game.gd
+++ no-more-summoning/scenes/game/game.gd
@@ -14,7 +14,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("quit"):
+		GameManager.load_main_scene()
 
 
 func _handle_level_changed():
